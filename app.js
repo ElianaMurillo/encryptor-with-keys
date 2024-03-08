955c670884f901f854c9055191e119fab320fa18
unknown <elianamuri213@gmail.com>
2024-03-08 15:12:32 -0500

    feature completion

--- app.js
+++ app.js
@@ -7,22 +7,19 @@ function asignarTextoElemento(elemento, texto) {
 function imagenInicial() {
     let imagenCifrado = document.createElement('img');
     imagenCifrado.src = "img/person.png";
-    imagenCifrado.classList.add('clasePrueba');
     let src = document.getElementById('imgCifradoInicial');
     src.appendChild(imagenCifrado);
 }
 
-/*function btnCopiar(){
-    let btnCopy = document.createElement('input');
-    btnCopy.setAttribute("type", "submit");
-    let src = document.getElementById('btnCopiarTexto');
-    src.appendChild(btnCopy);
-}*/
+function apareceBtnCopiar() {
+    document.getElementById("btnCopiarTexto").style.display = "block"
+}
 
 function condicionesIniciales() {
     asignarTextoElemento('.text-answer', 'Ningún mensaje fue encontrado');
     asignarTextoElemento('.text-indications', 'Ingresa el texto que deseas encriptar o desencriptar');
     imagenInicial();
+    document.getElementById("btnCopiarTexto").style.display = "none"
 }
 
 function cifrar() {
@@ -30,16 +27,15 @@ function cifrar() {
     //console.log(textoUsuario)
     //console.log(typeof(textoUsuario))
     let textoCifrado = textoUsuario
-    .replaceAll('e','enter')
-    .replaceAll('i','imes')
-    .replaceAll('a','ai')
-    .replaceAll('o','ober')
-    .replaceAll('u','ufat')
-    //document.getElementById('write-text').value = textoCifrado
+        .replaceAll('e', 'enter')
+        .replaceAll('i', 'imes')
+        .replaceAll('a', 'ai')
+        .replaceAll('o', 'ober')
+        .replaceAll('u', 'ufat')
     asignarTextoElemento('.text-answer', 'Texto encriptado')
     asignarTextoElemento('.text-indications', textoCifrado)
     document.getElementById('imgCifradoInicial').style.display = 'none';
-    //btnCopiar();
+    apareceBtnCopiar()    
 }
 
 function descifrar() {
@@ -47,14 +43,29 @@ function descifrar() {
     //console.log(textoUsuario)
     //console.log(typeof(textoUsuario))
     let textoDescifrado = textoUsuario
-    .replaceAll('enter', 'e')
-    .replaceAll('imes', 'i')
-    .replaceAll('ai', 'a')
-    .replaceAll('ober', 'o')
-    .replaceAll('ufat', 'u')
+        .replaceAll('enter', 'e')
+        .replaceAll('imes', 'i')
+        .replaceAll('ai', 'a')
+        .replaceAll('ober', 'o')
+        .replaceAll('ufat', 'u')
     asignarTextoElemento('.text-answer', 'Texto desencriptado')
     asignarTextoElemento('.text-indications', textoDescifrado)
     document.getElementById('imgCifradoInicial').style.display = 'none';
+    apareceBtnCopiar()
 }
+
+function copy (text) {
+    navigator.clipboard.writeText(text)
+}
+
+function copyTextClipboard () {
+    const elementToCopy = document.getElementById('answer-cifrado');
+    const textToCopy = elementToCopy.innerText;
+    console.log('llegué aquí')
+    console.log(textToCopy)
+    copy(textToCopy);
+    alert('El texto ha sido copiado.')
+}
+
 
 condicionesIniciales();
