f7707970343c1487fa34d1d631ee4eb9838cc0d7
unknown <elianamuri213@gmail.com>
2024-03-08 17:59:28 -0500

    put rules for the user text

--- app.js
+++ app.js
@@ -4,11 +4,43 @@ function asignarTextoElemento(elemento, texto) {
     return;
 }
 
-function imagenInicial() {
+/*function imagenInicial() {
     let imagenCifrado = document.createElement('img');
     imagenCifrado.src = "img/person.png";
     let src = document.getElementById('imgCifradoInicial');
     src.appendChild(imagenCifrado);
+}*/
+
+function imagenInicial() {
+    document.getElementById("imgCifradoInicial").style.display = "block"
+}
+
+function containsUppercase(str) {
+    const uppercaseLetters = /[A-Z]/;
+    return uppercaseLetters.test(str);
+}
+
+function transformUppercaseToLowcase() {
+    let transformToLowercaseText = document.getElementById('write-text').toLowerCase();
+    return transformToLowercaseText
+}
+
+function containsSpecialChars(str) {
+    const specialChars =
+        /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return specialChars.test(str);
+}
+
+function changeContentCifrar(src) {
+    let changeTextarea = document.getElementById("write-text");
+    changeTextarea.value = src.toLowerCase();
+    cifrar();
+}
+
+function changeContentDescifrar(src) {
+    let changeTextarea = document.getElementById("write-text");
+    changeTextarea.value = src.toLowerCase();
+    descifrar();
 }
 
 function apareceBtnCopiar() {
@@ -23,42 +55,68 @@ function condicionesIniciales() {
 }
 
 function cifrar() {
-    let textoUsuario = document.getElementById('write-text').value;
-    //console.log(textoUsuario)
-    //console.log(typeof(textoUsuario))
-    let textoCifrado = textoUsuario
-        .replaceAll('e', 'enter')
-        .replaceAll('i', 'imes')
-        .replaceAll('a', 'ai')
-        .replaceAll('o', 'ober')
-        .replaceAll('u', 'ufat')
-    asignarTextoElemento('.text-answer', 'Texto encriptado')
-    asignarTextoElemento('.text-indications', textoCifrado)
-    document.getElementById('imgCifradoInicial').style.display = 'none';
-    apareceBtnCopiar()    
+    let userText = document.getElementById('write-text').value;
+    if (containsSpecialChars(userText) == false && containsUppercase(userText) == false) {
+        let textoCifrado = userText
+            .replaceAll('e', 'enter')
+            .replaceAll('i', 'imes')
+            .replaceAll('a', 'ai')
+            .replaceAll('o', 'ober')
+            .replaceAll('u', 'ufat')
+        asignarTextoElemento('.text-answer', 'Texto encriptado')
+        asignarTextoElemento('.text-indications', textoCifrado)
+        document.getElementById('imgCifradoInicial').style.display = 'none';
+        apareceBtnCopiar();
+        document.getElementById("imgCifradoInicial").style.display = "none"
+    } else if (containsUppercase(userText) == true) {
+        console.log('Llegué aquí 1');
+        let askUserToLowerCase = confirm('El texto ingresado contiene mayúsculas, ¿desea pasarlo a minúsculas para poder encriptar el texto?')
+        if (askUserToLowerCase == true) {
+            changeContentCifrar(userText);
+        } else {
+            condicionesIniciales();
+            alert('Revise el texto ingresado, este tiene caracteres especiales');
+        }
+    } else {
+        condicionesIniciales();
+        alert('Revise el texto ingresado, este tiene caracteres especiales');
+    }
 }
 
 function descifrar() {
-    let textoUsuario = document.getElementById('write-text').value;
-    //console.log(textoUsuario)
-    //console.log(typeof(textoUsuario))
-    let textoDescifrado = textoUsuario
-        .replaceAll('enter', 'e')
-        .replaceAll('imes', 'i')
-        .replaceAll('ai', 'a')
-        .replaceAll('ober', 'o')
-        .replaceAll('ufat', 'u')
-    asignarTextoElemento('.text-answer', 'Texto desencriptado')
-    asignarTextoElemento('.text-indications', textoDescifrado)
-    document.getElementById('imgCifradoInicial').style.display = 'none';
-    apareceBtnCopiar()
+    let userText = document.getElementById('write-text').value;
+    if (containsSpecialChars(userText) == false && containsUppercase(userText) == false) {
+        let textoUsuario = document.getElementById('write-text').value;
+        let textoDescifrado = textoUsuario
+            .replaceAll('enter', 'e')
+            .replaceAll('imes', 'i')
+            .replaceAll('ai', 'a')
+            .replaceAll('ober', 'o')
+            .replaceAll('ufat', 'u')
+        asignarTextoElemento('.text-answer', 'Texto desencriptado')
+        asignarTextoElemento('.text-indications', textoDescifrado)
+        document.getElementById('imgCifradoInicial').style.display = 'none';
+        apareceBtnCopiar();
+        document.getElementById("imgCifradoInicial").style.display = "none"
+    } else if (containsUppercase(userText) == true) {
+        let askUserToLowerCase = confirm('El texto ingresado contiene mayúsculas, ¿desea pasarlo a minúsculas para poder desencriptar el texto?')
+        if (askUserToLowerCase == true) {
+            changeContentDescifrar(userText);
+        } else {
+            condicionesIniciales();
+            alert('Revise el texto ingresado, este tiene caracteres especiales');
+        }
+    } else {
+        condicionesIniciales();
+        alert('Revise el texto ingresado, este tiene caracteres especiales');
+    }
 }
 
-function copy (text) {
+function copy(text) {
     navigator.clipboard.writeText(text)
 }
 
-function copyTextClipboard () {
+function copyTextClipboard() {
     const elementToCopy = document.getElementById('answer-cifrado');
     const textToCopy = elementToCopy.innerText;
     console.log('llegué aquí')
@@ -66,6 +124,5 @@ function copyTextClipboard () {
     copy(textToCopy);
     alert('El texto ha sido copiado.')
 }
-
 
 condicionesIniciales();
